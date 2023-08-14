d6748657ff6fc8e6bad0244735929b0c7e3dab8b
NekKatya <baturina.ea@gmail.com>
2023-08-14 17:53:25 +0300

    9 неделя

---
+++ week 9/tasks/index.js
@@ -0,0 +1,76 @@
+/* 1 задание
+
+let salary1 = 4000;
+let salary2 = 22000;
+console.log(salary1+salary2)
+*/ 
+
+
+
+/* 2 задание
+
+let name = "Vladivostok";
+let n = 2000;
+console.log(name+' '+n)
+*/ 
+
+
+
+/* 3 задание
+
+let messeger = 128;
+let foto = 254;
+let VK = 137;
+let Youtube = 201;
+console.log((messeger+foto+VK+Youtube)/60)
+*/ 
+
+
+
+/* 4 задание
+
+let n = 2;
+let degree=5;
+console.log(2**5)
+*/
+
+
+
+/* 5 задание
+
+let text="Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log(text)
+*/
+
+
+
+/* 6 задание
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings+","+name)
+*/
+
+
+
+/* 7 задание
+
+let time;
+time=34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time=time-15;
+console.log('Немного сториз в соцсетях. Осталось минут: '+ time);
+time=time-10;
+console.log('Немного не новостей, но событий. Осталось минут: '+ time);
+time=0;
+console.log('Вы приехали. Добро пожаловать в Москву ')
+*/
+
+/* 8 задание
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`)
+*/
+
+
